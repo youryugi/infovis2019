0ff3607f6781a0b342af956ad3b9d2bf5c363dc3
youryugi <44858359+youryugi@users.noreply.github.com>
2019-04-17 14:17:43 +0900

    Update main.js

--- work/w04task2/main.js
+++ work/w04task2/main.js
@@ -12,34 +12,20 @@ function main()
     var camera = new THREE.PerspectiveCamera( fov, aspect, near, far );
     camera.position.set( 0, 0, 5 );
     scene.add( camera );
-    function guang()
-    {var light = new THREE.SpotLight(0xffffff)
-    light.position.set(1, 1, 1)
-    light.target = floor; // 投射方向指向地板
-    light.castShadow = true; // 用于产生阴影
-    scene.add(light)}
-    function wenli()
-    {cube.castShadow = true; 
-    // 对floor接受阴影
-    floor.receiveShadow = true;
-    // 光源设置
-    light.castShadow = true;
-    // 光源的阴影设置
-    //light.shadow.mapSize.width = 512;  // default
-    //light.shadow.mapSize.height = 512; // default     light.shadow.camera.near = 0.5;       // default
-    //light.shadow.camera.far = 500      // default
-        }
+    var light = new THREE.PointLight( 0xffffff );
+    light.position.set( 1, 1, 1 );
+    scene.add( light );
     var renderer = new THREE.WebGLRenderer();
     renderer.setSize( width, height );
     document.body.appendChild( renderer.domElement );
 
     var geometry = new THREE.BoxGeometry( 1, 1, 1 );
-    var material = new THREE.MeshBasicMaterial( { color: 0xffffff } );
+    var material = new THREE.MeshLambertMaterial( { color: 0x344542 } );
     var cube = new THREE.Mesh( geometry, material );
     scene.add( cube );
-    guang();
+  
     loop();
-    wenli();
+
 
     function loop()
     {
